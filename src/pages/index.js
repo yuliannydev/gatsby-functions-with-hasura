faee5cfea61bb48c9b1a224c190d7d2326b985c6
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-09 00:54:37 -0400

    :sparkles: Introduce API with Hasura
    The API hace one method POST, that method introduce a email adress in de DB on hasura cloud and heroku

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,30 @@ import React, { useState } from "react"
 
 const IndexPage = () => {
   const [email, setEmail] = useState(``);
-  const submit = (event) => {
+  const submit = async (event) => {
     event.preventDefault();
+    const response = await fetch(`https://sincere-heron-48.hasura.app/v1/graphql`, {
+      method: `POST`,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          mutation Insert_User($email: String) {
+            insert_User_one(object: {
+              email: $email
+            }) {
+              email
+            }
+          }
+          `,
+          variables: {
+            email
+          },
+      }),
+    })
+    await response.json();
     setEmail(``);
-    console.log({ email });
   }
   return (
     <main>
